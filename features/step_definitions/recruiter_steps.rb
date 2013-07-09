b484d0389aeb10e6f868bcbfd370e58ec62a8e84
James C. Holder <jholder@theladders.com>
2013-07-09 16:26:34 -0400

    Add initial Cucumber features.

---
+++ features/step_definitions/recruiter_steps.rb
@@ -0,0 +1,22 @@
+$:.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'labels'
+require 'recruiter'
+require 'job'
+
+Given(/^a Recruiter with Name "(.*?)"$/) do |name|
+  @recruiter = Recruiter.new(name: Name.new("Jane Doe"))
+end
+
+Given(/^an empty JobList$/) do
+  @joblist = JobList.new
+end
+
+When(/^the Recruiter posts a Job titled "(.*?)" of Type "(.*?)" to the JobList$/) do |title, type|
+  @job = Job.new(title: Title.new(title), type: Type.new(type))
+  @joblist.post(@job)
+end
+
+Then(/^the JobList should contain a Job titled "(.*?)" of Type "(.*?)"$/) do |title, type|
+  expect(@joblist.include?(@job)).to be_true
+end
+
