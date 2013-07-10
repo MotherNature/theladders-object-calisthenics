6f29eea8eda75ba47eac256d332f34476e976d3b
James C. Holder <jholder@theladders.com>
2013-07-10 10:39:51 -0400

    Describe feature by which Jobseekers save Jobs to a SavedJobsList.

---
+++ features/jobseekers_save.feature
@@ -0,0 +1,12 @@
+Feature: Save Job
+  In order to keep track of the jobs to which I want to apply
+  As a Jobseeker
+  I want to save Jobs to the Site
+
+  Scenario: Save Job
+    Given a Jobseeker with Name "Jane Doe"
+    And a Recruiter with Name "Robert Smith"
+    And an empty SavedJobsList
+    When the Recruiter posts a Job titled "Simple Job" of JobType "ATS" to the PostingList
+    And the Jobseeker saves the Job to the SavedJobsList
+    Then the SavedJobsList should contain the Job
---
+++ features/step_definitions/jobseeker_steps.rb
@@ -0,0 +1,32 @@
+$:.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'labels'
+require 'job'
+require 'jobseeker'
+require 'pp'
+
+Before do
+  @recruiters = []
+  @jobs = []
+  @joblist = nil
+  @postinglist = nil
+  @job = nil
+  @recruiter = nil
+  @jobtypefactory = JobTypeFactory.new
+end
+
+Given(/^a Jobseeker with Name "(.*?)"$/) do |name|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^an empty SavedJobsList$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the Jobseeker saves the Job to the SavedJobsList$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the SavedJobsList should contain the Job$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
