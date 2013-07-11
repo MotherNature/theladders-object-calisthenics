df0cdc8d98b6eb3b8795ebe1fb3bd198ec465b41
James C. Holder <jholder@theladders.com>
2013-07-11 13:59:49 -0400

    Add "List Jobs: it should be able to fetch a List of only those Jobs submitted by itself" spec to spec/recruiter_spec.rb.

--- spec/recruiter_spec.rb
+++ spec/recruiter_spec.rb
@@ -33,4 +33,24 @@ describe Recruiter do
       @postinglist.jobs_posted_by(@recruiter).should include job
     end
   end
+
+  describe "List Jobs" do
+    before(:each) do
+      title = "Example Title"
+      jobtype = "ATS"
+
+      @recruiter1 = Recruiter.new(name: Name.new("Rachel McExample"))
+      @recruiter2 = Recruiter.new(name: Name.new("Jake Sample"))
+      @job1 = Job.new(title: Title.new(title), jobtype: jobtypefactory.build_jobtype(jobtype))
+      @job2 = Job.new(title: Title.new(title), jobtype: jobtypefactory.build_jobtype(jobtype))
+
+      @postinglist.post_job(job: @job1, posted_by: @recruiter1)
+      @postinglist.post_job(job: @job2, posted_by: @recruiter2)
+    end
+
+    it "should be able to fetch a List of only those Jobs submitted by itself" do
+      filtered_joblist = @postinglist.jobs_posted_by(@recruiter1).should include @job1
+      filtered_joblist = @postinglist.jobs_posted_by(@recruiter2).should_not include @job1
+    end
+  end
 end
