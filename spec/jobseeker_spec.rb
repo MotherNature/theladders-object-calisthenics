955b475daba50843b2a22e8e8b00742fd43416ec
James C. Holder <jholder@theladders.com>
2013-07-11 15:37:20 -0400

    Refactor Jobseeker specs to use JobFactory instead of Job#new, thereby hiding the details of JobType and JobTypeFactory, in particular.

--- spec/jobseeker_spec.rb
+++ spec/jobseeker_spec.rb
@@ -1,4 +1,5 @@
 $:.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'spec'))
 
 require 'labels'
 require 'job'
@@ -6,64 +7,54 @@ require 'jobseeker'
 require 'recruiter'
 require 'posting'
 
-jobtypefactory = JobTypeFactory.new
-
 describe Jobseeker do
   before(:each) do
-    @postinglist = PostingList.new
-    @savedjobrecordlist = SavedJobRecordList.new
+      
+    @jobfactory = JobFactory.new
+    @jobapplicationrecordservice = JobApplicationRecordService.new
 
-    title = Title.new("Example Title")
-    jobtype = jobtypefactory.build_jobtype("ATS")
-    @job = Job.new(title: title, jobtype: jobtype)
-    
-    recruiter = Recruiter.new(name: Name.new("Rachel McExample"))
-    @postinglist.post_job(job: @job, posted_by: recruiter)
+    @savedjobrecordlist = SavedJobRecordList.new
 
     @jobseeker = Jobseeker.new(name: Name.new("Jane Doe"))
   end
 
   describe "Save Job" do
     it "should be able to save a List of Jobs" do
-      @savedjobrecordlist.save_job_for_jobseeker(job: @job, jobseeker: @jobseeker)
-      @savedjobrecordlist.jobs_saved_by(@jobseeker).should include @job
+      job = @jobfactory.build_job(title_string: "Example Title", jobtype_string: "ATS")
+      @savedjobrecordlist.save_job_for_jobseeker(job: job, jobseeker: @jobseeker)
+      @savedjobrecordlist.jobs_saved_by(@jobseeker).should include job
     end
   end
 
   describe "Apply to Job" do
     before(:each) do
       @jobapplicationlist = JobApplicationList.new
-      @jobapplicationrecordservice = JobApplicationRecordService.new
-
-      @title = Title.new("Example Title")
+      @postinglist = PostingList.new
 
       @recruiter = Recruiter.new(name: Name.new("Rachel McExample"))
       @jobseeker = Jobseeker.new(name: Name.new("Jane Doe"))
+
+      @jobapplication = JobApplication.new(jobseeker: @jobseeker)
     end
 
     it "should be able to apply to an ATS Job without a resume" do
-      jobtype = jobtypefactory.build_jobtype("ATS")
-      job = Job.new(title: @title, jobtype: jobtype)
+      job = @jobfactory.build_job(title_string: "Example Job", jobtype_string: "ATS")
       @postinglist.post_job(job: job, posted_by: @recruiter)
 
-      jobapplication = JobApplication.new(jobseeker: @jobseeker)
-      @jobapplicationrecordservice.apply_jobapplication_to_job(jobapplication: jobapplication, job: job)
+      @jobapplicationrecordservice.apply_jobapplication_to_job(jobapplication: @jobapplication, job: job)
       
-      @jobapplicationrecordservice.jobapplications_submitted_for_job(job).should include jobapplication
+      @jobapplicationrecordservice.jobapplications_submitted_for_job(job).should include @jobapplication
     end
 
     it "should not be able to apply to a JReq Job without a resume" do
-      # TODO: Refactor most of this spec into a helper method
-      jobtype = jobtypefactory.build_jobtype("JReq")
-      job = Job.new(title: @title, jobtype: jobtype)
+      job = @jobfactory.build_job(title_string: "Example Job", jobtype_string: "JReq")
       @postinglist.post_job(job: job, posted_by: @recruiter)
 
-      jobapplication = JobApplication.new(jobseeker: @jobseeker)
       expect {
-        @jobapplicationrecordservice.apply_jobapplication_to_job(jobapplication: jobapplication, job: job)
+        @jobapplicationrecordservice.apply_jobapplication_to_job(jobapplication: @jobapplication, job: job)
       }.to raise_error(InvalidJobApplicationError)
       
-      @jobapplicationrecordservice.jobapplications_submitted_for_job(job).should_not include jobapplication
+      @jobapplicationrecordservice.jobapplications_submitted_for_job(job).should_not include @jobapplication
     end
   end
 end
