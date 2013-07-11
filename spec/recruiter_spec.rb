6a7e99f3923cc8f98d6114cd3fff52cb8482df27
James C. Holder <jholder@theladders.com>
2013-07-11 13:46:26 -0400

    Implement "Post Jobs: should be able to a post simple ATS job" spec in spec/recruiter_spec.rb.

--- spec/recruiter_spec.rb
+++ spec/recruiter_spec.rb
@@ -1,12 +1,25 @@
 $:.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
 
+require 'labels'
 require 'recruiter'
+require 'posting'
+require 'job'
+
+jobtypefactory = JobTypeFactory.new
 
 describe Recruiter do
-  describe "Post Jobs" do
-    it "should be able to a post simple ATS job" do
-      pending "Starting file."
-    end
+  before(:each) do
+    @recruiter = Recruiter.new(name: Name.new("Rachel McExample"))
+    @postinglist = PostingList.new
   end
 
+  describe "Post Jobs" do
+    it "should be able to a post simple ATS job" do
+      title = Title.new("Example Job Title")
+      job = Job.new(title: Title.new(title), jobtype: jobtypefactory.build_jobtype("ATS"))
+      @postinglist.post_job(job: job, posted_by: @recruiter)
+
+      @postinglist.jobs_posted_by(@recruiter).should include job
+    end
+  end
 end
