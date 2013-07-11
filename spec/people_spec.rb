1b37225bb165ff205de559e605c286444d880418
James C. Holder <jholder@theladders.com>
2013-07-11 11:18:26 -0400

    Add spec for new Identity class, used to consolidate multiple aspects of a cross-system object's identity (e.g. Name and IDNumber).

--- spec/people_spec.rb
+++ spec/people_spec.rb
@@ -1,9 +1,22 @@
 $:.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
 
+require 'labels'
+
 describe IDNumber do
   it "should initialize with a String for the ID" do
     idnumber = IDNumber.new("ID0001")
 
     idnumber.should be
+  end
+end
+
+describe Identity do
+  it "should initialize with a Name and an IDNumber" do
+    name = Name.new("Jane Smith")
+    idnumber = IDNumber.new("ID0001")
+
+    identity = Identity.new(name: name, idnumber: idnumber)
+
+    identity.should be
   end
 end
