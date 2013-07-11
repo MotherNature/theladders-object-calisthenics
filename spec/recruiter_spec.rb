cd2b758edcea1a310635e18b9559daff43a698a3
James C. Holder <jholder@theladders.com>
2013-07-11 13:31:14 -0400

    Add preliminary Recruiter spec (RSpec) in anticipation of refactoring away the lower-level Cucumber feature descriptions.

---
+++ spec/recruiter_spec.rb
@@ -0,0 +1,12 @@
+$:.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
+
+require 'recruiter'
+
+describe Recruiter do
+  describe "Post Jobs" do
+    it "should be able to a post simple ATS job" do
+      pending "Starting file."
+    end
+  end
+
+end
