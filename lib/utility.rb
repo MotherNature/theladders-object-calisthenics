760fbaa0b16e7859d4b4c6464e8c1744bff1bdcd
James C. Holder <jholder@theladders.com>
2013-07-11 09:05:06 -0400

    Add List class to new lib/utility.rb in anticipation of simplifying all of the extant *List classes.

---
+++ lib/utility.rb
@@ -0,0 +1,23 @@
+class List
+  def initialize(list_items=[])
+    @list_items = list_items
+  end
+
+  def add(list_item)
+    @list_items.push(list_item)
+  end
+
+  def include?(list_item)
+    @list_items.include?(list_item)
+  end
+
+  private
+  def items_filtered_for(subject_item, &filter_block)
+    filtered_items = @items.select do |list_item|
+      filter_block.call(list_item)
+    end
+
+    self.class.new(filtered_items)
+  end
+end
+
