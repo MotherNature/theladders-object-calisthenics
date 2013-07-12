9ecaf48be88ab6f3d45c3b7a25ea7e0daf651fae
James C. Holder <jholder@theladders.com>
2013-07-12 11:12:31 -0400

    Add RecruiterList class, which supplies an ordered array of Recruiters.

--- lib/recruiters.rb
+++ lib/recruiters.rb
@@ -1,4 +1,17 @@
 require 'people'
 
 class Recruiter < Person
+  def name_to_string
+    @name.to_string
+  end
 end
+
+class RecruiterList < List
+  def to_array
+    recruiters = super
+    recruiters.sort_by do |recruiter|
+      recruiter.name_to_string
+    end
+  end
+end
+
