486078a6c90dcc35daa0c092d797822e1acc014e
James C. Holder <jholder@theladders.com>
2013-07-12 11:19:58 -0400

    Add RecruiterListReportGenerator and RecruiterListReport classes, fulfilling the new spec.

--- lib/recruiters.rb
+++ lib/recruiters.rb
@@ -7,3 +7,21 @@ class RecruiterList < List
   include ListOrderedByName
 end
 
+class RecruiterListReport
+  def initialize(recruiterlist)
+    @recruiterlist = recruiterlist
+  end
+
+  def to_string
+    name_strings = @recruiterlist.to_array.map do |recruiter|
+      recruiter.name_to_string
+    end
+    name_strings.join("\n")
+  end
+end
+
+class RecruiterListReportGenerator
+  def generate_from(recruiterlist)
+    recruiterlistreport = RecruiterListReport.new(recruiterlist)
+  end
+end
