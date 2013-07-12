8eb5ada4c0989568ef65a26651500bce39983a85
James C. Holder <jholder@theladders.com>
2013-07-12 10:12:29 -0400

    Add JobseekerList class, which supplies an ordered array of Jobseekers through #jobseekers_as_array.
    Add Jobseeker#name_as_string so that we can sort Jobseekers by their names.

--- lib/jobseekers.rb
+++ lib/jobseekers.rb
@@ -1,4 +1,17 @@
 require 'people'
+require 'utilities'
 
 class Jobseeker < Person
+  def name_as_string
+    @name.to_string
+  end
+end
+
+class JobseekerList < List
+  def jobseekers_as_array(sort_by: nil)
+    jobseekers = to_array
+    jobseekers.sort_by do |jobseeker|
+      jobseeker.name_as_string
+    end
+  end
 end
