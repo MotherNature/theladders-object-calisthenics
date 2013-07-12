8db94595393ec466bfb05b88ac301e2d88036c7f
James C. Holder <jholder@theladders.com>
2013-07-12 14:47:21 -0400

    Update posting_report_spec.rb so that its PostingList is actually populated with Postings (as opposed to Jobs).

--- spec/posting_report_spec.rb
+++ spec/posting_report_spec.rb
@@ -12,14 +12,22 @@ describe PostingListReportGenerator do
     @job2 = jobfactory.build_job(title_string: "Bench Warmer", jobtype_string: "ATS")
     @job3 = jobfactory.build_job(title_string: "Candy Tester", jobtype_string: "ATS")
 
+    @recruiter1 = Recruiter.new(name: Name.new("Alice Smith"))
+    @recruiter2 = Recruiter.new(name: Name.new("Brenda Long"))
+    @recruiter3 = Recruiter.new(name: Name.new("Cindi Maxton"))
+
+    @posting1 = Posting.new(job: @job1, posted_by: @recruiter1)
+    @posting2 = Posting.new(job: @job2, posted_by: @recruiter2)
+    @posting3 = Posting.new(job: @job3, posted_by: @recruiter3)
+
     @postinglistreportgenerator = PostingListReportGenerator.new
   end
 
   describe "Generate Posting Report" do
     it "should list Postings, showing their Job's Title and the Name of the Recruiter who posted it" do
-      postinglist = PostingList.new([@job1, @job2, @job3])
+      postinglist = PostingList.new([@posting1, @posting2, @posting3])
       postinglistreport = @postinglistreportgenerator.generate_from(postinglist)
-      postinglistreport.to_string.should == "Job Title: Applied Technologist\nRecruiter: Alice Smith\n---\nJob Title: Bench Warmer\nRecruiter: Brenda Long\n---\nJob Title: Candy Tester\nRecruiter: Cindi Bond"
+      postinglistreport.to_string.should == "Job Title: Applied Technologist\nRecruiter: Alice Smith\n---\nJob Title: Bench Warmer\nRecruiter: Brenda Long\n---\nJob Title: Candy Tester\nRecruiter: Cindi Maxton"
     end
   end
 end
