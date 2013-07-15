df03a44644dbe2aa2172eb05643609e6d37a53e1
James C. Holder <jholder@theladders.com>
2013-07-15 10:22:24 -0400

    Update jobseeker_spec.rb to reflect the pending refactoring of JobApplicationRecord, JobApplicationRecordList and JobApplicationRecordService to to tie Jobseekers to Postings instead of to Jobs.

--- spec/jobseeker_spec.rb
+++ spec/jobseeker_spec.rb
@@ -18,8 +18,13 @@ describe Jobseeker do
 
     @jobseeker = Jobseeker.new(name: Name.new("Jane Doe"))
 
+    @recruiter = Recruiter.new(name: Name.new("Rudy Smith"))
+
     @ats_job = @jobfactory.build_job(title_string: "Example Title", jobtype_string: "ATS")
     @jreq_job = @jobfactory.build_job(title_string: "Example Title", jobtype_string: "JReq")
+
+    @ats_job_posting = Posting.new(job: @ats_job, posted_by: @recruiter)
+    @jreq_job_posting = Posting.new(job: @jreq_job, posted_by: @recruiter)
   end
 
   describe "Save Job" do
@@ -30,54 +35,45 @@ describe Jobseeker do
   end
 
   describe "Apply to Job" do
-    before(:each) do
-      @jobapplicationlist = JobApplicationList.new
-      @postinglist = PostingList.new
+    it "should be able to apply to an ATS Job Posting without a resume" do
+      jobapplication = JobApplication.new(jobseeker: @jobseeker)
 
-      @recruiter = Recruiter.new(name: Name.new("Rachel McExample"))
-      @jobseeker = Jobseeker.new(name: Name.new("Jane Doe"))
-
-      @jobapplication = JobApplication.new(jobseeker: @jobseeker)
-
-      @postinglist.post_job(job: @ats_job, posted_by: @recruiter)
-      @postinglist.post_job(job: @jreq_job, posted_by: @recruiter)
-    end
-
-    it "should be able to apply to an ATS Job without a resume" do
-      @jobapplicationrecordservice.apply_jobapplication_to_job(jobapplication: @jobapplication, job: @ats_job)
+      @jobapplicationrecordservice.apply_jobapplication_to_posting(jobapplication: jobapplication, posting: @ats_job_posting)
       
-      @jobapplicationrecordservice.jobapplications_submitted_for_job(@ats_job).should include @jobapplication
+      @jobapplicationrecordservice.jobapplications_submitted_for_posting(@ats_job_posting).should include jobapplication
     end
 
-    it "should not be able to apply to a JReq Job without a resume" do
+    it "should not be able to apply to a JReq Job Posting without a resume" do
+      jobapplication = JobApplication.new(jobseeker: @jobseeker)
+
       expect {
-        @jobapplicationrecordservice.apply_jobapplication_to_job(jobapplication: @jobapplication, job: @jreq_job)
+        @jobapplicationrecordservice.apply_jobapplication_to_posting(jobapplication: jobapplication, posting: @jreq_job_posting)
       }.to raise_error(IncompatibleJobApplicationError)
-      
-      @jobapplicationrecordservice.jobapplications_submitted_for_job(@jreq_job).should_not include @jobapplication
+
+      @jobapplicationrecordservice.jobapplications_submitted_for_posting(@jreq_job_posting).should_not include jobapplication
     end
 
-    it "should be able to apply to a JReq Job with a resume" do
+    it "should be able to apply to a JReq Job Posting with a resume" do
       resume = Resume.new(jobseeker: @jobseeker)
       jobapplication = JobApplication.new(jobseeker: @jobseeker, resume: resume) 
 
-      @jobapplicationrecordservice.apply_jobapplication_to_job(jobapplication: jobapplication, job: @jreq_job)
+      @jobapplicationrecordservice.apply_jobapplication_to_posting(jobapplication: jobapplication, posting: @jreq_job_posting)
       
-      @jobapplicationrecordservice.jobapplications_submitted_for_job(@jreq_job).should include jobapplication
+      @jobapplicationrecordservice.jobapplications_submitted_for_posting(@jreq_job_posting).should include jobapplication
     end
 
-    it "should be able to apply to different Jobs with different Resumes" do
+    it "should be able to apply to different Jobs Posting with different Resumes" do
       resume1 = Resume.new(jobseeker: @jobseeker)
       resume2 = Resume.new(jobseeker: @jobseeker)
       
       jobapplication1 = JobApplication.new(jobseeker: @jobseeker, resume: resume1)
       jobapplication2 = JobApplication.new(jobseeker: @jobseeker, resume: resume2)
 
-      @jobapplicationrecordservice.apply_jobapplication_to_job(jobapplication: jobapplication1, job: @ats_job)
-      @jobapplicationrecordservice.apply_jobapplication_to_job(jobapplication: jobapplication2, job: @jreq_job)
+      @jobapplicationrecordservice.apply_jobapplication_to_posting(jobapplication: jobapplication1, posting: @ats_job_posting)
+      @jobapplicationrecordservice.apply_jobapplication_to_posting(jobapplication: jobapplication2, posting: @jreq_job_posting)
       
-      @jobapplicationrecordservice.jobapplications_submitted_for_job(@jreq_job).should include jobapplication1
-      @jobapplicationrecordservice.jobapplications_submitted_for_job(@jreq_job).should include jobapplication2
+      @jobapplicationrecordservice.jobapplications_submitted_for_posting(@jreq_job_posting).should include jobapplication1
+      @jobapplicationrecordservice.jobapplications_submitted_for_posting(@jreq_job_posting).should include jobapplication2
 
       jobapplication1.has_this_resume?(resume1).should be_true
       jobapplication1.has_this_resume?(resume2).should be_false
