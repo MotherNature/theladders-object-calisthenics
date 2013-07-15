5e044a9b1031ac57203882125615afb868eebbb0
James C. Holder <jholder@theladders.com>
2013-07-15 10:49:26 -0400

    Add complete spec for the basic String output of JobsAppliedToReportGenerator.

--- spec/jobseeker_report_spec.rb
+++ spec/jobseeker_report_spec.rb
@@ -35,16 +35,34 @@ describe JobsAppliedToReportGenerator do
   describe "Generate \"Jobs applied to\" Report" do
     before(:each) do
       @jobseeker = Jobseeker.new(name: Name.new("Alice Green"))
+      @other_jobseeker = Jobseeker.new(name: Name.new("Betty Smith"))
+      recruiter = Recruiter.new(name: Name.new("Rudy Zane"))
 
       jobfactory = JobFactory.new
 
       @job1 = jobfactory.build_job(title_string: "Applied Technologist", jobtype_string: "ATS")
       @job2 = jobfactory.build_job(title_string: "Bench Warmer", jobtype_string: "ATS")
       @job3 = jobfactory.build_job(title_string: "Candy Tester", jobtype_string: "ATS")
+
+      @posting1 = Posting.new(job: @job1, posted_by: recruiter)
+      @posting2 = Posting.new(job: @job2, posted_by: recruiter)
+      @posting3 = Posting.new(job: @job3, posted_by: recruiter)
+
+      @jobapplication = JobApplication.new(jobseeker: @jobseeker)
+      @other_jobapplication = JobApplication.new(jobseeker: @other_jobseeker)
+
+      @jobapplicationrecord1 = JobApplicationRecord.new(jobapplication: @jobapplication, posting: @posting1)
+      @jobapplicationrecord2 = JobApplicationRecord.new(jobapplication: @jobapplication, posting: @posting2)
+      @jobapplicationrecord3 = JobApplicationRecord.new(jobapplication: @jobapplication, posting: @posting3)
+
+      @jobsappliedtoreportgenerator = JobsAppliedToReportGenerator.new
     end
 
     it "should list Jobs applied to by the Jobseeker" do
-      pending
+      jobapplicationrecordlist = JobApplicationRecordList.new([@jobapplicationrecord1, @jobapplicationrecord2, @jobapplicationrecord3])
+
+      jobsappliedtoreport = @jobsappliedtoreportgenerator.generate_for_jobseeker_from_jobapplicationrecordlist(jobseeker: @jobseeker, jobapplicationrecordlist: jobapplicationrecordlist)
+      jobsappliedtoreport.to_string.should == "Title: Applied Technologist\nType: ATS\n---\nTitle: Bench Warmer\nType: ATS\n---\nTitle: Candy Tester\nType: ATS"
     end
   end
 end
