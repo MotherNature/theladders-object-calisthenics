b993c44c26c16e369d4c691de0f0f7ca1fdf15b0
James C. Holder <jholder@theladders.com>
2013-07-16 12:38:17 -0400

    Add spec for JobApplicationSubmissionList in anticipation of changing the behavior of #apply_jobapplication_to_posting.

---
+++ spec/jobapplicationsubmissionlist_spec.rb
@@ -0,0 +1,32 @@
+$:.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'spec'))
+
+require 'labels'
+require 'jobs'
+require 'examples'
+
+describe JobApplicationSubmissionList do
+  before(:each) do
+
+    examplefactory = ExampleFactory.new
+    
+    jobseeker = examplefactory.build_jobseeker
+    recruiter = examplefactory.build_recruiter
+    job = examplefactory.build_job
+
+    @posting = Posting.new(job: job, posted_by: recruiter)
+    @jobapplication = JobApplication.new(jobseeker: jobseeker)
+
+    @jobapplicationsubmissionlist = JobApplicationSubmissionList.new
+  end
+
+  # TODO: Ask whether this implicit return is a bad idea and, if so, for alternative approaches.
+  describe "#apply_jobapplication_to_posting" do
+    it "should return the JobApplicationSubmission created to record the posting activity" do
+      jobapplicationsubmission = @jobapplicationsubmissionlist.apply_jobapplication_to_posting(jobapplication: @jobapplication, posting: @posting)
+      jobapplicationsubmission.submitted_for_jobapplication?(@jobapplication).should be_true
+      jobapplicationsubmission.submitted_for_posting?(@posting).should be_true
+    end
+  end
+end
+
