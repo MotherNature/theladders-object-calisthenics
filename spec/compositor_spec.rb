f65cf0a4a516b0aeb6aa900d6b24c3d7ba0f1eb2
James C. Holder <jholder@theladders.com>
2013-07-17 09:52:27 -0400

    Refactor spec/compositor_spec.rb to remove duplicate initialization code.

--- spec/compositor_spec.rb
+++ spec/compositor_spec.rb
@@ -10,81 +10,65 @@ require 'resumes'
 require 'examples'
 require 'compositors'
 
-describe JobPoster do
+describe "Compositors" do
   before(:each) do
-    examplefactory = ExampleFactory.new
+    @examplefactory = ExampleFactory.new
 
-    @recruiter = examplefactory.build_recruiter
+    @jobseeker = @examplefactory.build_jobseeker
+    @recruiter = @examplefactory.build_recruiter
 
-    @job = examplefactory.build_job
+    @job = @examplefactory.build_job
 
+    @jobapplicationlist = JobApplicationList.new
     @postinglist = PostingList.new
-  end
-
-  describe "#post_job" do
-    it "should return a Posting" do
-      jobposter = JobPoster.new(recruiter: @recruiter, postinglist: @postinglist)
-
-      posting = jobposter.post_job(@job)
-
-      [posting.class, *posting.class.ancestors].should include(Posting)
-    end
-  end
-end
-
-describe JobApplicationPreparer do
-  before(:each) do
-    examplefactory = ExampleFactory.new
-
-    @jobseeker = examplefactory.build_jobseeker
-
-    @jobapplicationlist = JobApplicationList.new
-  end
-
-  describe "#prepare_application" do
-    it "should return a JobApplication" do
-      jobapplicationpreparer = JobApplicationPreparer.new(jobseeker: @jobseeker, jobapplicationlist: @jobapplicationlist)
-
-      jobapplication = jobapplicationpreparer.prepare_application
-
-      [jobapplication.class, *jobapplication.class.ancestors].should include(JobApplication)
-    end
-  end
-end
-
-describe JobApplicationSubmitter do
-  before(:each) do
-    examplefactory = ExampleFactory.new
-
-    @recruiter = examplefactory.build_recruiter
-
-    @job = examplefactory.build_job
-
-    @jobseeker = examplefactory.build_jobseeker
-
-    @jobapplicationlist = JobApplicationList.new
-
-    jobapplicationpreparer = JobApplicationPreparer.new(jobseeker: @jobseeker, jobapplicationlist: @jobapplicationlist)
-
-    @jobapplication = jobapplicationpreparer.prepare_application
 
     @jobapplicationsubmissionservice = JobApplicationSubmissionService.new
-
-    @postinglist = PostingList.new
-
-    jobposter = JobPoster.new(recruiter: @recruiter, postinglist: @postinglist)
-
-    @posting = jobposter.post_job(@job)
   end
 
-  describe "#submit_application" do
-    it "should return a JobApplicationSubmission" do
-      jobapplicationsubmitter = JobApplicationSubmitter.new(jobapplication: @jobapplication, jobapplicationsubmissionservice: @jobapplicationsubmissionservice)
+  describe JobPoster do
+    describe "#post_job" do
+      it "should return a Posting" do
+        jobposter = JobPoster.new(recruiter: @recruiter, postinglist: @postinglist)
+
+        posting = jobposter.post_job(@job)
+
+        [posting.class, *posting.class.ancestors].should include(Posting)
+      end
+    end
+  end
+
+  describe JobApplicationPreparer do
+    describe "#prepare_application" do
+      it "should return a JobApplication" do
+        jobapplicationpreparer = JobApplicationPreparer.new(jobseeker: @jobseeker, jobapplicationlist: @jobapplicationlist)
+
+        jobapplication = jobapplicationpreparer.prepare_application
+
+        [jobapplication.class, *jobapplication.class.ancestors].should include(JobApplication)
+      end
+    end
+  end
+
+  describe JobApplicationSubmitter do
+    before(:each) do
+      jobapplicationpreparer = JobApplicationPreparer.new(jobseeker: @jobseeker, jobapplicationlist: @jobapplicationlist)
+
+      @jobapplication = jobapplicationpreparer.prepare_application
+
+      jobposter = JobPoster.new(recruiter: @recruiter, postinglist: @postinglist)
+
+      @posting = jobposter.post_job(@job)
+    end
+
+    describe "#submit_application" do
+      it "should return a JobApplicationSubmission" do
+        jobapplicationsubmitter = JobApplicationSubmitter.new(jobapplication: @jobapplication, jobapplicationsubmissionservice: @jobapplicationsubmissionservice)
 
 
-      jobapplicationsubmission = jobapplicationsubmitter.submit_application(@posting)
+        jobapplicationsubmission = jobapplicationsubmitter.submit_application(@posting)
 
-      [jobapplicationsubmission.class, *jobapplicationsubmission.class.ancestors].should include(JobApplicationSubmission)
+        [jobapplicationsubmission.class, *jobapplicationsubmission.class.ancestors].should include(JobApplicationSubmission)
+      end
     end
   end
 end
