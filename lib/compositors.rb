b5fc39f08f20bcb7c65deedea5a453b3754b481f
James C. Holder <jholder@theladders.com>
2013-07-17 09:26:24 -0400

    Fulfill spec for JobPoster#post_job.

--- lib/compositors.rb
+++ lib/compositors.rb
@@ -4,4 +4,14 @@ require 'recruiters'
 require 'postings'
 
 class JobPoster
+  def initialize(recruiter: nil, postinglist:  nil)
+    @recruiter = recruiter
+    @postinglist = postinglist
+  end
+
+  def post_job(job)
+    posting = Posting.new(job: job, posted_by: @recruiter)
+    @postinglist.add(posting)
+    posting
+  end
 end
