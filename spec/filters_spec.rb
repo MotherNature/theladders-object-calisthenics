7ba59c45446f67b22b96dd36d71d5f2e1937988c
James C. Holder <jholder@theladders.com>
2013-07-30 13:40:12 -0400

    Move the specs for filter usage into its own filters_spec.rb file.

---
+++ spec/filters_spec.rb
@@ -0,0 +1,58 @@
+describe "Combine Filterers" do
+  before(:each) do
+    examplefactory = ExampleFactory.new
+
+    @submissionservice = SubmissionService.new
+
+    @jobseeker1 = examplefactory.build_jobseeker
+    @jobseeker2 = examplefactory.build_jobseeker
+
+    @recruiter1 = examplefactory.build_recruiter
+    @recruiter2 = examplefactory.build_recruiter
+
+    @job1 = examplefactory.build_job
+    @job2 = examplefactory.build_job
+    @job3 = examplefactory.build_job
+
+    @posting1 = Posting.new(job: @job1, posted_by: @recruiter1)
+    @posting2 = Posting.new(job: @job2, posted_by: @recruiter1)
+    @posting3 = Posting.new(job: @job3, posted_by: @recruiter2)
+
+    @application1 = Application.new(jobseeker: @jobseeker1)
+    @application2 = Application.new(jobseeker: @jobseeker1)
+    @application3 = Application.new(jobseeker: @jobseeker2)
+
+    @submission1 = @submissionservice.apply_application_to_posting(application: @application1, posting: @posting1)
+    @submission2 = @submissionservice.apply_application_to_posting(application: @application2, posting: @posting2)
+    @submission3 = @submissionservice.apply_application_to_posting(application: @application3, posting: @posting3)
+
+    @datetime1 = DateTime.new(2013, 7, 12, 0, 0, 0)
+    @datetime2 = DateTime.new(2013, 8, 13, 0, 0, 0)
+    @datetime3 = DateTime.new(2013, 9, 14, 0, 0, 0)
+    @datetime4 = DateTime.new(2013, 10, 15, 0, 0, 0)
+
+    @submissionrecord1 = SubmissionRecord.new(submission: @submission1, recorded_at_datetime: @datetime1)
+    @submissionrecord2 = SubmissionRecord.new(submission: @submission2, recorded_at_datetime: @datetime2)
+    @submissionrecord3 = SubmissionRecord.new(submission: @submission2, recorded_at_datetime: @datetime3)
+    @submissionrecord4 = SubmissionRecord.new(submission: @submission3, recorded_at_datetime: @datetime4)
+    @submissionrecord5 = SubmissionRecord.new(submission: @submission3, recorded_at_datetime: @datetime1)
+
+    @submissionrecordlist = SubmissionRecordList.new([@submissionrecord1, @submissionrecord2, @submissionrecord3, @submissionrecord4])
+  end
+
+  describe "Find just Jobseekers who applied to Jobs posted by a given Recruiter on a given Date" do
+    it "should return a list of Jobseekers who have applied to Jobs on the given Date" do
+      date = @datetime1.to_date
+      datefilterer = DateSubmissionRecordFilterer.new(date: date, submissionrecordlist: @submissionrecordlist)
+      recruiterfilterer = RecruiterSubmissionRecordFilterer.new(recruiter: @recruiter1, submissionrecordlist: @submissionrecordlist)
+
+      list1 = datefilterer.jobseekers
+      list2 = recruiterfilterer.jobseekers
+
+      jobseekerlist = list1.to_array & list2.to_array
+
+      jobseekerlist.should include(@jobseeker1)
+      jobseekerlist.should_not include(@jobseeker2)
+    end
+  end
+end
