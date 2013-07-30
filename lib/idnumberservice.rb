3570c7aaa419e4e59a807804da77952a29b0666d
James C. Holder <jholder@theladders.com>
2013-07-30 16:27:15 -0400

    Fulfill preliminary spec for IDNumberService.

---
+++ lib/idnumberservice.rb
@@ -0,0 +1,4 @@
+class IDNumberService
+  def initialize
+  end
+end
--- spec/idnumberservice_spec.rb
+++ spec/idnumberservice_spec.rb
@@ -1,6 +1,8 @@
 $:.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
 $:.unshift(File.join(File.dirname(__FILE__), '..', 'spec'))
 
+require 'idnumberservice'
+
 describe IDNumberService do
   it "can be initialized" do
     IDNumberService.new
