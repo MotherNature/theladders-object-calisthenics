da7171c3eb4fa4932affad74c0007884698aee70
James C. Holder <jholder@theladders.com>
2013-08-05 12:29:24 -0400

    Add jobseeker filter spec. Expand specs.
    Add spec testing whether the report filters for just those jobs applied to by the given jobseeker.
    
    Expand the very first spec to include the classes used by the new spec.

--- spec/theladders_report_spec.rb
+++ spec/theladders_report_spec.rb
@@ -7,17 +7,46 @@ Dir[File.join(File.dirname(__FILE__), '..', 'lib', 'reports', '*.rb')].each do |
   require report_lib
 end
 
+require 'jobs'
+require 'jobseekers'
+
 describe "Jobseekers should be able to see a listing of the jobs for which they have applied" do
   before(:each) do
+    @jobseeker = Jobseeker.new
+    @other_jobseeker = Jobseeker.new
   end
 
   describe JobseekerApplicationsReport do
     it "should list the jobs to which a given jobseeker has applied" do
-      reportgenerator = JobseekerApplicationsReportGenerator.new
+      job1 = Job.new(title: "Valid Job 1")
+      job2 = Job.new(title: "Valid Job 2")
 
-      report = reportgenerator.generate_from(nil)
+      @jobseeker.apply_to(job: job1)
+      @jobseeker.apply_to(job: job2)
+
+      list = JobseekerList.new([@jobseeker, @other_jobseeker]) 
+
+      reportgenerator = JobseekerApplicationsReportGenerator.new(@jobseeker)
+
+      report = reportgenerator.generate_from(list)
 
       report.to_string.should == "Valid Job 1\nValid Job 2"
+    end
+
+    it "should only list the jobs to which a given jobseeker has applied" do
+      valid_job = Job.new(title: "Valid Job")
+      @jobseeker.apply_to(job: valid_job)
+
+      invalid_job = Job.new(title: "Invalid Job")
+      @other_jobseeker.apply_to(job: invalid_job)
+
+      list = JobseekerList.new([@jobseeker, @other_jobseeker]) 
+
+      reportgenerator = JobseekerApplicationsReportGenerator.new(@jobseeker)
+
+      report = reportgenerator.generate_from(list)
+
+      report.to_string.should == "Valid Job"
     end
   end
 end
