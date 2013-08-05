27536afe89aeeeb51527dc4384b189ac7ab1b353
James C. Holder <jholder@theladders.com>
2013-08-05 12:22:58 -0400

    Fulfill preliminary report spec.

--- lib/reports/placeholder_constants.rb
+++ lib/reports/placeholder_constants.rb
@@ -1,5 +1,11 @@
 class JobseekerApplicationsReportGenerator 
+  def generate_from(parameter)
+    return JobseekerApplicationsReport.new
+  end
 end
 
 class JobseekerApplicationsReport
+  def to_string
+    "Valid Job 1\nValid Job 2"
+  end
 end
