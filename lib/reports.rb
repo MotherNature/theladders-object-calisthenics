14c48aeb5d99a0c3e844d60908530aa8da8c30b1
James C. Holder <jholder@theladders.com>
2013-08-08 12:19:29 -0400

    Add Reports module to lib/reports.rb.
    Add Reports module, which will be used to consolidate reporting code and in hopes of later taking reporting functionality out of the data classes entirely.

---
+++ lib/reports.rb
@@ -0,0 +1,32 @@
+require 'utilities'
+
+module Reports
+  def self.included(base_class)
+    base_class.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def reports(method_name, &block)
+      full_method_symbol = "report_#{method_name}".to_sym
+      define_method(full_method_symbol, &block)
+    end
+  end
+
+  def report(reportable)
+    report_methods = public_methods.select do |method_symbol|
+      method_symbol.to_s =~ /^report_/
+    end
+
+    report_methods.each do |method_symbol|
+      method_symbol.to_s =~ /^report_(.*)/
+      reporting_on = $1
+      
+      full_method_symbol = "display_#{reporting_on}".to_sym
+
+      if(reportable.respond_to?(full_method_symbol))
+        reportable.send(full_method_symbol, send(method_symbol))
+      end
+    end
+  end
+end
+
