a868d5397eee91eef3734accf7b4eba8913d4461
James C. Holder <jholder@theladders.com>
2013-08-08 10:16:32 -0400

    Add job-saving spec.
    Add spec checking whether a jobseeker can save a job (after being assigned the role of a jobseeker).

---
+++ spec/save_jobs_spec.rb
@@ -0,0 +1,27 @@
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'spec'))
+
+require 'jobs'
+require 'jobseekers'
+require 'employers'
+require 'submissions'
+
+describe "Jobseekers can save jobs onsite for later viewing" do
+  before(:each) do
+    @jobseeker = Jobseeker.new
+    @jobseeker = JobSaver.with_role_performed_by(@jobseeker)
+
+    employer = Employer.new(name: "Erin Employ")
+    employer = JobPoster.with_role_performed_by(employer)
+
+    unposted_job = UnpostedJob.new(title: "A Job", type: JobType.ATS)
+
+    @job = employer.post_job(unposted_job)
+  end
+
+  describe Jobseeker do
+    it "should be able to save a list of jobs" do
+      @jobseeker.save_job(@job)
+    end
+  end
+end
