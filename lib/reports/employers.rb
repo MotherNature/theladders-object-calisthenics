61c985aab9b400649c2c25c1081a4c8dcbb6552a
James C. Holder <jholder@theladders.com>
2013-08-12 09:59:27 -0400

    Fulfill expanded job report specs.
    Fulfill expanded specs for EmployersApplyingJobseekersByJobReportGenerator. Its #generate_from now returns an instance of the new JobseekersAndJobsListReport report, which lists both jobseekers and the jobs to which they applied.

--- lib/reports/employers.rb
+++ lib/reports/employers.rb
@@ -1,5 +1,8 @@
 require 'jobs'
+require 'jobseekers'
 require 'employers'
+require 'reports/jobs'
+require 'reports/jobseekers'
 
 class EmployersPostedJobReportGenerator
   def initialize(employer)
@@ -21,6 +24,47 @@ class EmployersApplyingJobseekersByJobReportGenerator
 
   def generate_from(jobseekerlist)
     only_applied_to_employers_jobs_jobseekers = AppliedToEmployersJobsJobApplierList.filtered_from(jobapplierlist: jobseekerlist, employer: @employer)
-    JobseekerListReport.new(only_applied_to_employers_jobs_jobseekers)
+    JobseekersAndJobsListReport.new(only_applied_to_employers_jobs_jobseekers)
+  end
+end
+
+class JobseekersAndJobsListReport < Report
+  include JobseekerStringFormatter
+  include JobStringFormatter
+
+  def initialize(list)
+    @list = list
+    @jobseeker_names = []
+    @titles = []
+    @employer_names = []
+  end
+
+  reports_on :jobs
+
+  when_reporting :jobseeker_name do |name|
+    @jobseeker_names.push(name)
+  end
+
+  when_reporting :employer_name do |name|
+    @employer_names.push(name)
+  end
+
+  when_reporting :job_title do |name|
+    @titles.push(name)
+  end
+
+  def to_string
+    @list.report_to(self)
+
+    entries = [ ]
+    (0...@jobseeker_names.size).each do |index|
+      rows = [ ]
+      rows.push(jobseeker_properties_as_string(jobseeker_name: @jobseeker_names[index]))
+      rows.push(job_properties_as_string(job_title: @titles[index], employer_name: @employer_names[index]))
+      entry = rows.join("\n")
+      entries.push(entry)
+    end
+
+    entries.join("\n---\n")
   end
 end
