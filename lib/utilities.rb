6b0133b4a6455f3a0d7ed06d95ea0b00686b8b67
James C. Holder <jholder@theladders.com>
2013-08-13 13:43:26 -0400

    Add List class.
    Add List class in anticipation of having all other Lists inherit from it.

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -24,3 +24,46 @@ class RoleDelegator < SimpleDelegator
     self.assign_role_to(redirectee)
   end
 end
+
+class List
+  def initialize(list=[])
+    @list = list
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+
+  def select(&block)
+    filtered_items = @list.select(&block)
+    self.class.new(filtered_items)
+  end
+
+  def size
+    @list.size
+  end
+
+  def with(new_item)
+    self.class.new([*@list, new_item])
+  end
+
+  def any?(&block)
+    filtered_list = select(&block)
+    filtered_list.size > 0
+  end
+
+  def report_to(reportable)
+    self.each do |item|
+      item.report_to(reportable)
+    end
+  end
+
+  def filtered_by(filters)
+    filtered_items = @list.select do |item|
+      filters.all? do |filter|
+        item.passes_filter?(filter)
+      end
+    end
+    self.class.new(filtered_items)
+  end
+end
