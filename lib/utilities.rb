6932198d69c48ae31015e3d285cd9d30ad118507
James C. Holder <jholder@theladders.com>
2013-08-13 13:48:25 -0400

    Remove unused RoleDelegator class.

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -12,19 +12,6 @@ module HumanReadableDelegation
   end
 end
 
-class RoleDelegator < SimpleDelegator
-  include HumanReadableDelegation
-
-  def self.assign_role_to(redirectee)
-    self.new(redirectee)
-  end
-
-  # TODO: Can I make this a proper alias?
-  def self.with_role_performed_by(redirectee)
-    self.assign_role_to(redirectee)
-  end
-end
-
 class List
   def initialize(list=[])
     @list = list
