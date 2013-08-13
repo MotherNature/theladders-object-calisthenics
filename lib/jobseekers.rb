6b935adb8c631a57610ca5fdca13a4aa378d9958
James C. Holder <jholder@theladders.com>
2013-08-13 13:46:35 -0400

    JobseekerList inherits from List.
    Make JobseekerList a child of List, removing all redundant functionality.

--- lib/jobseekers.rb
+++ lib/jobseekers.rb
@@ -18,43 +18,5 @@ class Jobseeker
   end
 end
 
-class JobseekerList
-  def initialize(jobseekers)
-    @jobseekers = jobseekers
-  end
-
-  def size
-    @jobseekers.size
-  end
-
-  def each(&block)
-    @jobseekers.each(&block)
-  end
-
-  def select(&block)
-    JobseekerList.new(@jobseekers.select(&block))
-  end
-
-  def with(jobseeker)
-    JobseekerList.new([*@jobseekers, jobseeker])
-  end
-
-  def include?(jobseeker)
-    @jobseekers.include?(jobseeker)
-  end
-
-  def filtered_by(filters)
-    filtered_jobseekers = @jobseekers.select do |jobseeker|
-      filters.all? do |filter|
-        jobseeker.passes_filter?(filter)
-      end
-    end
-    JobseekerList.new(filtered_jobseekers)
-  end
-
-  def report_to(reportable)
-    each do |jobseeker|
-      jobseeker.report_to(reportable)
-    end
-  end
+class JobseekerList < List
 end
