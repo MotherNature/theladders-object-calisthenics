1f05fb7fd839ce9ed46ed99ea29938a616b8e00a
James C. Holder <jholder@theladders.com>
2013-08-14 11:43:19 -0400

    Fulfill more-jobseekers report spec.
    Fulfill HTMLJobseekerListReport spec, confirming that its output includes jobseekers added to the initial test list.
    
    Make HTMLJobseekerListReport a child of Report.

--- lib/reports/html.rb
+++ lib/reports/html.rb
@@ -1,20 +1,31 @@
 require 'jobseekers'
 
-class HTMLJobseekerReport
+class HTMLJobseekerReport < Report
   def initialize(jobseeker)
+    @name = ""
+    jobseeker.report_to(self)
+  end
+
+  upon_receiving :jobseeker_name do |name|
+    @name = name
   end
 
   def render
-    %{<div class="jobseeker"><span class="name">Betsy J. Basic</span></div>}
+    %{<div class="jobseeker"><span class="name">#{@name}</span></div>}
   end
 end
 
 class HTMLJobseekerListReport
   def initialize(jobseekers)
+    @sub_reports = jobseekers.map do |jobseeker|
+      HTMLJobseekerReport.new(jobseeker)
+    end
   end
 
   def render
-    %{<div class="jobseeker"><span class="name">Betsy J. Basic</span></div>} +
-    %{<div class="jobseeker"><span class="name">Anne Ditional</span></div>}
+    rendered_parts = @sub_reports.map do |report|
+      report.render
+    end
+    rendered_parts.join
   end
 end
