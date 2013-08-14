c6d9266c3b92ee87f5863ae9910ad506944092ca
James C. Holder <jholder@theladders.com>
2013-08-14 15:01:55 -0400

    Add TextUnpostedJobReport class.

--- lib/reports/text.rb
+++ lib/reports/text.rb
@@ -1,5 +1,11 @@
 require 'jobseekers'
 
+class TextUnpostedJobReport < UnpostedJobReport
+  def report
+    "Job[Title: #{job_title}]"
+  end
+end
+
 class TextPostedJobReport < PostedJobReport
   def report
     "Job[Title: #{job_title}][Employer: #{poster_name}]"
