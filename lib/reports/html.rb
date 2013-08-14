24eed2f6ccd63dd1647676244e43bd01a318ec4c
James C. Holder <jholder@theladders.com>
2013-08-14 11:33:10 -0400

    Fulfill HTMLJobseekerReport spec.
    Add HTMLJobseekerReport class.
    
    Fulfill preliminary spec for HTMLJobseekerReport.

---
+++ lib/reports/html.rb
@@ -0,0 +1,10 @@
+require 'jobseekers'
+
+class HTMLJobseekerReport
+  def initialize(jobseeker)
+  end
+
+  def render
+    %{<div class="jobseeker"><span class="name">Betsy J. Basic</span></div>}
+  end
+end
