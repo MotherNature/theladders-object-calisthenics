128feec11583369888577c7571dfeedb60ca8243
James C. Holder <jholder@theladders.com>
2013-08-14 09:27:12 -0400

    Remove unused SubmissionList class.

--- lib/submissions.rb
+++ lib/submissions.rb
@@ -9,40 +9,6 @@ class Submission
   end
 end
 
-class SubmissionList
-  def initialize(submissions=[])
-    @submissions = submissions
-  end
-
-  def each(&block)
-    @submissions.each(&block)
-  end
-
-  def select(&block)
-    filtered_submissions = @submissions.select(&block)
-    submissionList.new(filtered_submissions)
-  end
-
-  def any?(&block)
-    filtered_list = select(&block)
-    filtered_list.size > 0
-  end
-
-  def size
-    @submissions.size
-  end
-
-  def with(submission)
-    submissionList.new([*@submissions, submission])
-  end
-
-  def report_to(reportable)
-    self.each do |submission|
-      submission.report_to(reportable)
-    end
-  end
-end
-
 class WrongJobseekersResumeSubmission < Submission
   def valid?
     false
