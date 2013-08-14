498cd4f667e344f1285e88b2ca90455508db1e60
James C. Holder <jholder@theladders.com>
2013-08-14 10:46:31 -0400

    Add pending HTML report spec.

---
+++ spec/html_report_spec.rb
@@ -0,0 +1,27 @@
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'spec'))
+
+$:.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib', 'reports'))
+
+Dir[File.join(File.dirname(__FILE__), '..', 'lib', 'reports', '*.rb')].each do |report_lib|
+  require report_lib
+end
+
+require 'labels'
+require 'jobs'
+require 'jobseekers'
+require 'employers'
+require 'resumes'
+require 'submissions'
+
+require 'helpers'
+
+RSpec.configure do |klass|
+  klass.include Helpers
+end
+
+describe HTMLJobseekerReport do
+  it "should report on the jobseeker's name" do
+    pending
+  end
+end
