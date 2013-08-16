dfdd8d89786bcd378e93922df4fb57e9f938e486
James C. Holder <jholder@theladders.com>
2013-08-16 16:13:53 -0400

    Remove unused classes and modules.
    Remove unused JobseekerStringFormatter, JobseekerReport, and JobseekerListReport.

--- lib/reports/jobseekers.rb
+++ lib/reports/jobseekers.rb
@@ -1,51 +1,5 @@
 require 'jobs'
 
-module JobseekerStringFormatter
-  def jobseeker_properties_as_string(jobseeker_name: nil)
-    "Jobseeker[Name: #{jobseeker_name}]"
-  end
-end
-
-class JobseekerReport < Report
-  include JobseekerStringFormatter
-
-  def initialize(jobseeker)
-    @name = ""
-    jobseeker.report_to(self)
-  end
-
-  upon_receiving :jobseeker_name do |name|
-    @name = name
-  end
-
-  def render
-    jobseeker_properties_as_string(jobseeker_name: jobseeker_name)
-  end
-
-  private
-
-  def jobseeker_name
-    @name
-  end
-end
-
-class JobseekerListReport < Report
-  def initialize(list)
-    @list = list
-    @sub_reports = @list.map do |jobseeker|
-      JobseekerReport.new(jobseeker)
-    end
-  end
-
-  def render
-    formatted_strings = @sub_reports.map do |report|
-      report.render
-    end
-
-    formatted_strings.join("\n")
-  end
-end
-
 class JobseekerApplicationsReportGenerator 
   def initialize(jobseeker, report_class: nil)
     @jobseeker = jobseeker
