3b04aad24f2c159d21062329631cc8ffe1ab4ac3
James C. Holder <jholder@theladders.com>
2013-08-16 09:39:45 -0400

    Add #as_reportable to resumes.
    Add #as_reportable to Resume.
    
    Add ::as_reportable to NoResume.

--- lib/resumes.rb
+++ lib/resumes.rb
@@ -10,10 +10,18 @@ class Resume
   def belongs_to?(jobseeker)
     @jobseeker == jobseeker
   end
+
+  def as_reportable
+    OpenStruct.new(exists: exists?)
+  end
 end
 
 class NoResume
   def self.exists?
     false
+  end
+
+  def self.as_reportable
+    OpenStruct.new(exists: exists?)
   end
 end
