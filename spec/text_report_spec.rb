60c678cfae975a59f6b5be06761668b34b7ea9f6
James C. Holder <jholder@theladders.com>
2013-08-16 10:52:15 -0400

    Move report spec before renaming.
    Move JobseekerAndJobsReport spec to spec/text_report_spec.rb in anticipation of renaming and refactoring it to be more specific.
    
    Remove the now spec-less spec/theladders_report_spec.rb.

--- spec/text_report_spec.rb
+++ spec/text_report_spec.rb
@@ -21,6 +21,65 @@ RSpec.configure do |klass|
   klass.include Helpers
 end
 
+describe "TheLadders should be able to get a report of what jobseekers have applied to jobs on any given day" do
+  describe JobseekerAndJobsReport do
+    it "should list the given jobseeker and all of the jobs to which they have applied" do
+      report = JobseekerAndJobsReport.new(@jobseeker)
+      report.render.should == @default_report
+    end
+
+    before(:each) do
+      @jobseeker = applying_jobseeker
+
+      @job = posted_job
+
+      @jobseeker.apply_to(job: @job)
+
+      @default_report = "Jobseeker[Name: Jane Jobseek]\nJob[Title: A Job][Employer: Erin Employ]"
+    end
+    
+    def default_report_and(additional_string=nil)
+      @default_report + "\n---\n" + additional_string
+    end
+  end
+
+  describe JobseekersAndJobsListReport do
+    it "should list the given jobseekers and all of the jobs to which they have applied" do
+      report = JobseekersAndJobsListReport.new(@jobseekerlist)
+      report.render.should == @default_report
+    end
+
+    it "should list the given jobseeker and all of the jobs to which they have applied, including just-added jobseekers" do
+      new_jobseeker = applying_jobseeker(name: "Anne Nother")
+      new_jobseeker.apply_to(job: @job)
+
+      expanded_list = @jobseekerlist.with(new_jobseeker)
+      report = JobseekersAndJobsListReport.new(expanded_list)
+      report.render.should == @default_report + "\n---\nJobseeker[Name: Anne Nother]\nJob[Title: A Job][Employer: Erin Employ]"
+    end
+
+    it "should list the given jobseeker and all of the jobs to which they have applied, including just-applied-to jobs" do
+      new_job = posted_job(title: "Another Job")
+      @jobseeker.apply_to(job: new_job)
+
+      report = JobseekersAndJobsListReport.new(@jobseekerlist)
+      report.render.should == @default_report + "\nJob[Title: Another Job][Employer: Erin Employ]"
+    end
+
+    before(:each) do
+      @jobseeker = applying_jobseeker
+
+      @job = posted_job
+
+      @jobseeker.apply_to(job: @job)
+
+      @jobseekerlist = JobseekerList.new([@jobseeker])
+
+      @default_report = "Jobseeker[Name: Jane Jobseek]\nJob[Title: A Job][Employer: Erin Employ]"
+    end
+  end
+end
+
 describe "Employers should be able to see a listing of the jobs they have posted" do
   describe TextEmployersPostedJobReportGenerator do
     it "should generate a report that lists the jobs posted by an employer, including just-added ones" do
