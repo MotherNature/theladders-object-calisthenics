3d275f3a254e9fbb4bd372046418f5ec37ed84b0
James C. Holder <jholder@theladders.com>
2013-08-19 11:11:29 -0400

    Remove method/variable ambiguity in spec.

--- spec/role_spec.rb
+++ spec/role_spec.rb
@@ -16,12 +16,13 @@ end
 describe JobSaverRole do
   it "can save even if its role-taker has taken on additional roles" do
     expect do
-      jobseeker = applying_jobseeker
-      saving_jobseeker = JobSaverRole.new(roletaker: jobseeker, save_to_repo: JobRepo.new)
+      initial_jobseeker = applying_jobseeker
+      wrapped_jobseeker = JobSaverRole.new(roletaker: initial_jobseeker, save_to_repo: JobRepo.new)
+
       job = posted_job
 
-      saving_jobseeker.apply_to_job(job: job)
-      saving_jobseeker.save_job(job)
+      wrapped_jobseeker.apply_to_job(job: job)
+      wrapped_jobseeker.save_job(job)
     end.to_not raise_error
   end
 end
@@ -29,12 +30,13 @@ end
 describe JobApplierRole do
   it "can apply even if its role-taker has taken on additional roles" do
     expect do
-      jobseeker = saving_jobseeker
-      applying_jobseeker = JobApplierRole.new(roletaker: jobseeker, apply_to_service: ApplicationService.new)
+      initial_jobseeker = saving_jobseeker
+      wrapped_jobseeker = NewJobApplier.new(roletaker: initial_jobseeker, apply_to_service: ApplicationService.new)
+
       job = posted_job
 
-      applying_jobseeker.save_job(job)
-      applying_jobseeker.apply_to_job(job: job)
+      wrapped_jobseeker.save_job(job)
+      wrapped_jobseeker.apply_to_job(job: job)
     end.to_not raise_error
   end
 end
